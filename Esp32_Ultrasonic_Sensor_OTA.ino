d8c73b4d9660fd463202d199e8f5bd2e2f8cb6fc
Your NameMtheto <mthetosinjani@gmail.com>
2020-01-27 19:49:52 +0200

    c++ file

---
+++ Esp32_Ultrasonic_Sensor_OTA.ino
@@ -0,0 +1,245 @@
+// defines pins numbers
+
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <Update.h>
+
+const char* mqtt_server = "192.168.4.2";
+const char* host = "esp32";
+long lastMsg = 0;
+char msg[20];
+const int trigPin = 5;
+const int echoPin = 2;
+
+// defines variables
+float initialdistance = 200; // insert initial distance or distance between sensor and empty base tank
+float duration, distance, heightofwater, volume, lvolume;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+WebServer server(80);
+
+/*
+ * Login page
+ */
+
+const char* loginIndex = 
+ "<form name='loginForm'>"
+    "<table width='20%' bgcolor='A09F9F' align='center'>"
+        "<tr>"
+            "<td colspan=2>"
+                "<center><font size=4><b>ESP32 Login Page</b></font></center>"
+                "<br>"
+            "</td>"
+            "<br>"
+            "<br>"
+        "</tr>"
+        "<td>Username:</td>"
+        "<td><input type='text' size=25 name='userid'><br></td>"
+        "</tr>"
+        "<br>"
+        "<br>"
+        "<tr>"
+            "<td>Password:</td>"
+            "<td><input type='Password' size=25 name='pwd'><br></td>"
+            "<br>"
+            "<br>"
+        "</tr>"
+        "<tr>"
+            "<td><input type='submit' onclick='check(this.form)' value='Login'></td>"
+        "</tr>"
+    "</table>"
+"</form>"
+"<script>"
+    "function check(form)"
+    "{"
+    "if(form.userid.value=='ghii' && form.pwd.value=='winning1')"
+    "{"
+    "window.open('/serverIndex')"
+    "}"
+    "else"
+    "{"
+    " alert('Error Password or Username')/*displays error message*/"
+    "}"
+    "}"
+"</script>";
+ 
+/*
+ * Server Index Page
+ */
+ 
+const char* serverIndex = 
+"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
+"<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
+   "<input type='file' name='update'>"
+        "<input type='submit' value='Update'>"
+    "</form>"
+ "<div id='prg'>progress: 0%</div>"
+ "<script>"
+  "$('form').submit(function(e){"
+  "e.preventDefault();"
+  "var form = $('#upload_form')[0];"
+  "var data = new FormData(form);"
+  " $.ajax({"
+  "url: '/update',"
+  "type: 'POST',"
+  "data: data,"
+  "contentType: false,"
+  "processData:false,"
+  "xhr: function() {"
+  "var xhr = new window.XMLHttpRequest();"
+  "xhr.upload.addEventListener('progress', function(evt) {"
+  "if (evt.lengthComputable) {"
+  "var per = evt.loaded / evt.total;"
+  "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
+  "}"
+  "}, false);"
+  "return xhr;"
+  "},"
+  "success:function(d, s) {"
+  "console.log('success!')" 
+ "},"
+ "error: function (a, b, c) {"
+ "}"
+ "});"
+ "});"
+ "</script>";
+
+
+void mqttconnect() {
+  /* Loop until reconnected */
+  while (!client.connected()) {
+    Serial.print("MQTT connecting ...");
+    /* client ID */
+    String clientId = "ESP32Client";
+    /* connect now */
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      /* subscribe topic with default QoS 0*/
+      client.subscribe("ultra/readings");
+    } else {
+      Serial.print("failed, status code =");
+      Serial.print(client.state());
+      Serial.println("try again in 5 seconds");
+      /* Wait 5 seconds before retrying */
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(9600); // Starts the serial communication
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  WiFi.softAP("GS-Water-System", "winning1");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.softAPIP());
+  client.setServer(mqtt_server, 1883);
+  /* this receivedCallback function will be invoked 
+  when client received subscribed topic */
+ 
+  
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+
+/*use mdns for host name resolution*/
+  if (!MDNS.begin(host)) { //http://esp32.local
+    Serial.println("Error setting up MDNS responder!");
+    while (1) {
+      delay(1000);
+    }
+  }
+  Serial.println("mDNS responder started");
+  /*return index page which is stored in serverIndex */
+  server.on("/", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", loginIndex);
+  });
+  server.on("/serverIndex", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", serverIndex);
+  });
+  /*handling uploading firmware file */
+  server.on("/update", HTTP_POST, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+    ESP.restart();
+  }, []() {
+    HTTPUpload& upload = server.upload();
+    if (upload.status == UPLOAD_FILE_START) {
+      Serial.printf("Update: %s\n", upload.filename.c_str());
+      if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_WRITE) {
+      /* flashing firmware to ESP*/
+      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_END) {
+      if (Update.end(true)) { //true to set the size to the current progress
+        Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+      } else {
+        Update.printError(Serial);server.handleClient();
+      }
+    }
+  });
+  server.begin();
+}
+
+void loop() {
+  
+server.handleClient();
+// Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+//Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+
+  // Convert the time into a distance
+  distance = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+  heightofwater = initialdistance-distance;
+  volume = heightofwater*3.142*8281; //calculating volume of water in tank (Pi * r * r * h)
+  lvolume = volume/1000;              //Converting from cubic centimeters to Litres
+  Serial.print(distance);
+  Serial.print("cm");
+  Serial.println();
+  Serial.print(lvolume);
+  Serial.print("L");
+  Serial.println();
+
+  /* if client was disconnected then try to reconnect again */
+  if (!client.connected()) {
+    mqttconnect();
+  }
+
+    /* this function will listen for incomming 
+  subscribed topic-process-invoke receivedCallback */
+  client.loop();
+
+   /* publish the first message */
+   snprintf(msg, 10,"%6.2f", lvolume);
+   client.publish("ultra/readings",msg);
+   delay(1000);
+
+   /* publish the Second message */
+   snprintf(msg, 10,"%6.2f", lvolume);
+   client.publish("ultra/readings",msg);
+   delay(1000);
+
+   /* publish the Third message */
+   snprintf(msg, 10,"%6.2f", lvolume);
+   client.publish("ultra/readings",msg);
+   delay(58000);
+ 
+}
