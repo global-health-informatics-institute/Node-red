9e19911398e9cdd35e6e15c5863ecfc74e8fd00e
Your NameMtheto <mthetosinjani@gmail.com>
2020-02-19 18:34:14 +0200

    upload file in esp32

---
+++ Esp32_Ultrasonic_Sensor_Ease_V7.1.ino
@@ -0,0 +1,152 @@
+
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "GHII-GS";
+const char* password = "winning1";
+const char* mqtt_server = "192.168.100.4";
+
+//set your Static IP Address
+IPAddress local_IP(192, 168, 100, 5);
+
+//set your Gateway IP Address
+IPAddress gateway(192, 168, 100, 1);
+
+//set your Subnet Mask
+IPAddress subnet(255, 255, 255, 0);
+
+IPAddress primaryDNS(8, 8, 8, 8);
+IPAddress secondaryDNS(8, 8, 4, 4);
+
+// defines pins
+const int trigPin = 5;
+const int echoPin = 2;
+
+// defines variables
+long lastMsg = 0;
+char msg[20];
+float duration, distance;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void mqttconnect() {
+  /* Loop until reconnected */
+  while (!client.connected()) {
+    Serial.print("MQTT connecting ...");
+    /* client ID */
+    String clientId = "ESP32Client";
+    /* connect now */
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      /* subscribe topic with default QoS 0*/
+      client.subscribe("ultra/readings");
+    } else {
+      Serial.print("failed, status code =");
+      Serial.print(client.state());
+      Serial.println("try again in 5 seconds");
+      /* Wait 5 seconds before retrying */
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+
+  //configures static IP Address
+  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+    Serial.println("STA Failed to configure");
+  }
+  Serial.println(WiFi.localIP());
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    /* configure the MQTT server with IPaddress and port */
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  client.setServer(mqtt_server, 1883);
+  /* this receivedCallback function will be invoked
+    when client received subscribed topic */
+
+
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); // Starts the serial communication
+}
+
+float readings() {
+  /* if client was disconnected then try to reconnect again */
+  if (!client.connected()) {
+    mqttconnect();
+  }
+  /* this function will listen for incomming
+    subscribed topic-process-invoke receivedCallback */
+  client.loop();
+
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  //Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+
+  // Convert the time into a distance
+  distance = (duration / 2) / 29.1;   // Divide by 29.1 or multiply by 0.0343
+  //  Serial.print(distance);
+  //  Serial.println("cm");
+  return distance;
+}
+
+void loop() {
+  Serial.println(WiFi.localIP());
+  int has_10_readings = 0;
+  float sum_of_valid_reading = 0;
+  float average;
+
+  while (true) {
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+      Serial.println("IP address: ");
+      Serial.println(WiFi.localIP());
+
+      /* configure the MQTT server with IPaddress and port */
+    }
+    float current_reading = readings();
+    if (current_reading > 12 & current_reading < 176) {
+      has_10_readings++;
+      if (has_10_readings > 10) break;
+      if (has_10_readings > 1)
+        sum_of_valid_reading += current_reading;
+      Serial.println(has_10_readings);
+      Serial.println(current_reading);
+      Serial.println();
+    }
+    delay(500);
+  }
+
+
+  snprintf(msg, 10, "%6.2f", (sum_of_valid_reading / 10));
+  client.publish("ultra/readings", msg);
+  average = sum_of_valid_reading / 10;
+  Serial.print("Average: ");
+  Serial.println(average);
+  delay(50000);
+
+}
